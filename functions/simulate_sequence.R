0b428f320001d9a8ac7db47522c1872a2b05360e
michbur <michalburdukiewicz@gmail.com>
2016-07-02 22:54:32 +0200

    improved simulation of sequences

--- functions/simulate_sequence.R
+++ functions/simulate_sequence.R
@@ -1,16 +1,34 @@
 library(biogram)
 
 # alphabet
-alph <- as.character(1L:4)
+alph <- as.character(2L:4)
 
-sim_seq <- function(len, u)
+sim__single_seq <- function(len, u)
   sample(u, size = len, replace = TRUE)
 
-add_motif <- function(motif, motif_len, seq, len) {
-  motif_pos <- sample(1L:(len - motif_len), 1)
-  seq[motif_pos:(motif_pos + motif_len - 1)] <- motif
+add_single_motif <- function(motif, motif_len, seq, len) {
+  motif_pos <- sample(1L:(len - motif_len + 1), 1)
+  ids <- 0L:(motif_len - 1)
+  # exclude gaps
+  ids <- ids[motif != "_"] + motif_pos
+  seq[ids] <- motif[motif != "_"]
   seq
 }
 
-tmp <- sim_seq(16, alph)
-add_motif(c("1", "1", "1"), 3, tmp, 16)
+tmp <- sim__single_seq(16, alph)
+add_single_motif(c("1", "_", "1"), 3, tmp, 16)
+
+simulate_sequences <- function(n_seq, len, u, motif, fraction = 0.5) {
+  motif_len <- length(motif)
+  n_pos <- round(fraction*n_seq, 0)
+  
+  t(cbind(sapply(1L:n_pos, function(dummy)
+    add_single_motif(motif, motif_len, sim__single_seq(len, u), len)
+    ),
+    sapply(1L:(n_seq - n_pos), function(dummy)
+      sim__single_seq(len, u)
+      )
+  ))
+}
+
+simulate_sequences(50, 6, alph, c("1", "_", "1"))
