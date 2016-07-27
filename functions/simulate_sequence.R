dc10ab141e765287264d05a049eaa74f31ae94a5
michbur <michalburdukiewicz@gmail.com>
2016-07-27 11:15:57 +0200

    expanded simulation

--- functions/simulate_sequence.R
+++ functions/simulate_sequence.R
@@ -1,4 +1,5 @@
 library(biogram)
+library(dplyr)
 
 sim__single_seq <- function(len, u)
   sample(u, size = len, replace = TRUE)
@@ -21,18 +22,19 @@ simulate_sequences <- function(n_seq, len, u, motif_l, fraction = 0.5) {
   t(cbind(sapply(1L:n_pos, function(dummy) {
     motif <- sample(motif_l, 1)[[1]]
     add_single_motif(motif, length(motif), sim__single_seq(len, u), len)
-    }),
-    sapply(1L:(n_seq - n_pos), function(dummy)
-      sim__single_seq(len, u)
-      )
+  }),
+  sapply(1L:(n_seq - n_pos), function(dummy)
+    sim__single_seq(len, u)
+  )
   ))
 }
 
 generate_single_motif <- function(u) {
-  ns <- c(rep(2, 4), rep(3, 3))
-  ds <- list(0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0))
+  ns <- c(rep(2, 6), rep(3, 9))
+  ds <- c(as.list(0L:5), 
+          expand.grid(0L:2, 0L:2)  %>% split(1L:nrow(.)))
   
-  ngram_id <- sample(1L:7, 1)
+  ngram_id <- sample(1L:length(ns), 1)
   
   n <- ns[ngram_id]
   d <- ds[[ngram_id]]
@@ -46,11 +48,7 @@ generate_motif <- function(u, n_motif) {
   lapply(1L:n_motif, function(dummy) generate_single_motif(u))
 }
 
-# number of positive and negative sequences
-n_seq <- 500
-  
-# length of sequence
-l_seq <- 6
+
 
 # define alphabet
 alph <- as.character(1L:4)
@@ -64,17 +62,29 @@ test_quipt <- function(n_seq, l_seq) {
   test_dat <- simulate_sequences(n_seq*2, l_seq, alph, motif_l = motifs)
   
   # perform QuiPT
-  test_res <- test_features(binarize(count_multigrams(test_dat,
-                                                      ns = c(1, rep(2, 4), rep(3, 3)), 
-                                                      ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0)),
-                                                      u = alph)), 
-                            target = c(rep(1, n_seq), rep(0, n_seq)))
+  test_res <- count_multigrams(test_dat, 
+                               ns = c(1, rep(2, 6), rep(3, 9)), 
+                               ds = c(0, as.list(0L:5), 
+                                      expand.grid(0L:2, 0L:2)  %>% split(1L:nrow(.))),
+                               u = alph) %>% 
+    binarize() %>% 
+    test_features(target = c(rep(1, n_seq), rep(0, n_seq)), features = .)
   res_df <- data.frame(test_res)
   
-  res_df[["motif"]] <- res_df[["ngram"]] %in% code_ngrams(sapply(motifs, paste0, collapse = ""))
-  res_df
+  data.frame(#n_seq = n_seq, 
+             #l_seq = l_seq, 
+             res_df, 
+             motif = res_df[["ngram"]] %in% code_ngrams(sapply(motifs, paste0, collapse = "")),
+             p.value.adj = p.adjust(res_df[["p.value"]], "BH"))
 }
 
-test_quipt(500, 6)
+
+# number of positive and negative sequences
+n_seq <- 500
+
+# length of sequence
+l_seq <- 8
+
+res <- test_quipt(n_seq, l_seq)
 
 # analyse quipt must consider p-value adjustment
